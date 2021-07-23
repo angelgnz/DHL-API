3594b90a8d50397c02d69a214ee7cccc4beb29ee
angelgnz <69648541+angelgnz@users.noreply.github.com>
2021-07-22 19:48:18 -0500

    Actualización para uso FPEV

--- DHL/Entity/Base.php
+++ DHL/Entity/Base.php
@@ -19,10 +19,11 @@
  * Project:     DHL API
  *
  * @author      Al-Fallouji Bashar
- * @version     0.1
+ * @version     0.2
  */
 
 namespace DHL\Entity;
+
 use DHL\Datatype\Base as BaseDataType;
 
 /**
@@ -65,9 +66,14 @@ abstract class Base extends BaseDataType
             'required' => true,
         ),
     );
+    /**
+     * Parameters to be used in the header
+     * @var array
+     */
+    protected $header_meta_params = [];
 
     /**
-     * Parameters to be used in the body 
+     * Parameters to be used in the body
      * @var array
      */
     protected $_bodyParams = array();
@@ -94,7 +100,7 @@ abstract class Base extends BaseDataType
      * @var string
      * The schema version
      */
-    protected $_schemaVersion = '1.0';
+    protected $_schemaVersion = '6.2';
 
     /**
      * @var boolean
@@ -103,7 +109,7 @@ abstract class Base extends BaseDataType
     protected $_displaySchemaVersion = false;
 
     /**
-     * Parent node name of the object 
+     * Parent node name of the object
      * @var string
      */
     protected $_xmlNodeName = null;
@@ -116,10 +122,10 @@ abstract class Base extends BaseDataType
 
     /**
      * Class constructor
-     */ 
+     */
     public function __construct()
     {
-        $this->_params = array_merge($this->_headerParams, $this->_bodyParams);
+        $this->_params = array_merge($this->_headerParams, $this->header_meta_params, $this->_bodyParams);
         $this->initializeValues();
     }
 
@@ -127,7 +133,7 @@ abstract class Base extends BaseDataType
      * Generates the XML to be sent to DHL
      *
      * @param \XMLWriter $xmlWriter XMl Writer instance
-     *   
+     *
      * @return string
      */
     public function toXML(\XMLWriter $xmlWriter = null)
@@ -138,68 +144,59 @@ abstract class Base extends BaseDataType
         $xmlWriter->openMemory();
         $xmlWriter->setIndent(true);
         $xmlWriter->startDocument('1.0', 'UTF-8');
-            
+
         $xmlWriter->startElement('req:' . $this->_serviceName);
         $xmlWriter->writeAttribute('xmlns:req', self::DHL_REQ);
         $xmlWriter->writeAttribute('xmlns:xsi', self::DHL_XSI);
-        $xmlWriter->writeAttribute('xsi:schemaLocation', self::DHL_REQ . ' ' .$this->_serviceXSD);
-    
-        if ($this->_displaySchemaVersion) 
-        {
+        $xmlWriter->writeAttribute('xsi:schemaLocation', self::DHL_REQ . ' ' . $this->_serviceXSD);
+
+        if ($this->_displaySchemaVersion) {
             $xmlWriter->writeAttribute('schemaVersion', $this->_schemaVersion);
         }
 
-        if (null !== $this->_xmlNodeName) 
-        {
+        if (null !== $this->_xmlNodeName) {
             $xmlWriter->startElement($this->_xmlNodeName);
         }
 
         $xmlWriter->startElement('Request');
         $xmlWriter->startElement('ServiceHeader');
-        foreach ($this->_headerParams as $name => $infos) 
-        {
+        foreach ($this->_headerParams as $name => $infos) {
             $xmlWriter->writeElement($name, $this->$name);
         }
-        $xmlWriter->endElement(); // End of Request
         $xmlWriter->endElement(); // End of ServiceHeader
 
-        foreach ($this->_bodyParams as $name => $infos) 
-        {
-            if ($this->$name)
-            {
-                if (is_object($this->$name)) 
-                {
+        if (!empty($this->header_meta_params)) {
+            $xmlWriter->startElement('MetaData');
+            foreach ($this->header_meta_params as $name => $infos) {
+                $xmlWriter->writeElement($name, $this->$name);
+            }
+            $xmlWriter->endElement(); // End of MetaData
+        }
+        $xmlWriter->endElement(); // End of Request
+
+        foreach ($this->_bodyParams as $name => $infos) {
+            if ($this->$name) {
+                if (is_object($this->$name)) {
                     $this->$name->toXML($xmlWriter);
-                }
-                elseif (is_array($this->$name)) 
-                {
-                    if ('string' == $this->_params[$name]['type'])
-                    {
-                        foreach ($this->$name as $subelement)
-                        {
+                } elseif (is_array($this->$name)) {
+                    if ('string' == $this->_params[$name]['type']) {
+                        foreach ($this->$name as $subelement) {
                             $xmlWriter->writeElement($name, $subelement);
                         }
-                    }
-                    else
-                    {
-                        if (!isset($this->_params[$name]['disableParentNode']) || false == $this->_params[$name]['disableParentNode']) 
-                        {              
+                    } else {
+                        if (!isset($this->_params[$name]['disableParentNode']) || false == $this->_params[$name]['disableParentNode']) {
                             $xmlWriter->startElement($name);
                         }
 
-                        foreach ($this->$name as $subelement) 
-                        {
+                        foreach ($this->$name as $subelement) {
                             $subelement->toXML($xmlWriter);
                         }
 
-                        if (!isset($this->_params[$name]['disableParentNode']) || false == $this->_params[$name]['disableParentNode']) 
-                        {              
+                        if (!isset($this->_params[$name]['disableParentNode']) || false == $this->_params[$name]['disableParentNode']) {
                             $xmlWriter->endElement();
                         }
                     }
-                }
-                else
-                {
+                } else {
                     $xmlWriter->writeElement($name, $this->$name);
                 }
             }
@@ -208,85 +205,71 @@ abstract class Base extends BaseDataType
         $xmlWriter->endElement(); // End of parent node
 
         // End of class name tag
-        if (null !== $this->_xmlNodeName) 
-        {
+        if (null !== $this->_xmlNodeName) {
             $xmlWriter->endElement();
         }
 
         $xmlWriter->endDocument();
-    
+
         return $xmlWriter->outputMemory(true);
     }
 
     /**
      * Initialize object from an XML string
-     * 
+     *
      * @param string $xml XML String
-     * 
+     *
      * @return void
      * @throws \Exception Exception thrown if response returned has an error
      */
-    public function initFromXML($xml) 
+    public function initFromXML($xml)
     {
         $xml = simplexml_load_string(str_replace('req:', '', $xml));
 
-        if ((string) $xml->Response->Status->Condition->ConditionCode != '')
-        {
-            $errorMsg = ((string) $xml->Response->Status->Condition->ConditionCode) . ' : ' . ((string) $xml->Response->Status->Condition->ConditionData);
-            throw new \Exception('Error returned from DHL webservice : ' . $errorMsg);
+        if ((string) $xml->Response->Status->Condition->ConditionCode != '') {
+            $errorMsg = ((string) $xml->Response->Status->Condition->ConditionCode) . ' - ' . ((string) $xml->Response->Status->Condition->ConditionData);
+            throw new \Exception('DHL webService : ' . $errorMsg);
         }
 
         $parts = explode('\\', get_class($this));
         $className = array_pop($parts);
-        foreach ($xml->children() as $child) 
-        {           
+        foreach ($xml->children() as $child) {
             $childName = $child->getName();
-            switch ($childName)
-            {
+            switch ($childName) {
                 case 'Response':
                     $this->MessageTime = (string) $child->ServiceHeader->MessageTime;
                     $this->MessageReference = (string) $child->ServiceHeader->MessageReference;
                     $this->SiteID = (string) $child->ServiceHeader->SiteID;
                     $this->Password = '#';
-                break;
+                    break;
 
                 default:
-                    if (is_object($this->$childName))
-                    {
+                    if (is_object($this->$childName)) {
                         $this->$childName->initFromXml($child->asXML());
-                    }
-                    elseif (isset($this->_params[$childName]['multivalues']) && $this->_params[$childName]['multivalues'])
-                    {
-                        foreach ($child->children() as $subchild) 
-                        {
+                    } elseif (isset($this->_params[$childName]['multivalues']) && $this->_params[$childName]['multivalues']) {
+                        foreach ($child->children() as $subchild) {
                             $subchildName = $subchild->getName();
-                            if ($subchild->count() > 1)
-                            {
+                            if ($subchild->count() > 1) {
                                 $subchildName .= 's';
                             }
 
                             $childClassname = implode('\\', $parts) . '\\' . $this->_params[$subchildName]['type'];
                             $childClassname = str_replace('Entity', 'Datatype', $childClassname);
 
-                            if ('string' == $this->_params[$subchildName]['type'] && ($subchild->count() <= 1))
-                            {
+                            if ('string' == $this->_params[$subchildName]['type'] && ($subchild->count() <= 1)) {
                                 $childObj = trim((string) $subchild);
-                            }
-                            else
-                            {
+                            } else {
                                 $childObj = new $childClassname();
                                 $childObj->initFromXml($subchild->asXML());
                             }
-                            
+
                             $addMethodName = 'add' . ucfirst($subchildName);
                             $this->$addMethodName($childObj);
                         }
-                    }
-                    elseif (isset($this->$childName))
-                    {
+                    } elseif (isset($this->$childName)) {
                         $this->$childName = trim((string) $child);
                     }
-                break;
+                    break;
             }
         }
     }
@@ -297,16 +280,11 @@ abstract class Base extends BaseDataType
      */
     protected function initializeValues()
     {
-        foreach ($this->_params as $name => $infos) 
-        {
-            if (!$this->_isSubobject && isset($infos['subobject']) && $infos['subobject'])
-            {
-                if (isset($infos['multivalues']) && $infos['multivalues']) 
-                {
+        foreach ($this->_params as $name => $infos) {
+            if (!$this->_isSubobject && isset($infos['subobject']) && $infos['subobject']) {
+                if (isset($infos['multivalues']) && $infos['multivalues']) {
                     $this->_values[$name] = array();
-                }
-                else
-                {
+                } else {
                     $tmp = get_class($this);
                     $parts = explode('\\', $tmp);
                     array_pop($parts);
@@ -314,9 +292,7 @@ abstract class Base extends BaseDataType
                     $className = str_replace('Entity', 'Datatype', $className);
                     $this->_values[$name] = new $className();
                 }
-            }
-            else
-            {
+            } else {
                 $this->_values[$name] = null;
             }
         }
@@ -324,38 +300,30 @@ abstract class Base extends BaseDataType
 
     /**
      * Validate all parameters
-     * 
+     *
      * @return boolean True upon success
      * @throws \InvalidArgumentException Throws exception if type not valid or if value are missing
      */
     protected function validateParameters()
     {
-        foreach ($this->_params as $name => $infos) 
-        {
-            if (isset($infos['required']) && true === $infos['required'] && $this->_values[$name] === null)
-            {
+        foreach ($this->_params as $name => $infos) {
+            if (isset($infos['required']) && true === $infos['required'] && $this->_values[$name] === null) {
                 throw new \InvalidArgumentException('Field ' . $name . ' has no value');
             }
 
-            if ($this->_values[$name]) 
-            {
-                if (is_array($this->_values[$name])) 
-                {
-                    foreach ($this->_values[$name] as $subelement)
-                    {
+            if ($this->_values[$name]) {
+                if (is_array($this->_values[$name])) {
+                    foreach ($this->_values[$name] as $subelement) {
                         $subelement->validateParameters();
                     }
-                }
-                else 
-                {
+                } else {
                     $this->validateParameterType($name, $this->_values[$name]);
                     $this->validateParameterValue($name, $this->_values[$name]);
                 }
             }
         }
 
-        if (null === $this->_serviceName)
-        {
+        if (null === $this->_serviceName) {
             throw new \InvalidArgumentException('Class ' . get_class($this) . ' must have a valid serviceName property defined');
         }
 
